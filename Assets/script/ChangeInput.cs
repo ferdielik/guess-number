b963da13433df9e95c5e0be7cd97cb944320eddc
Ferdi Elik <elik.ferdi@gmail.com>
2018-09-27 20:02:17 +0300

    simple game structure

--- Assets/script/ChangeInput.cs
+++ Assets/script/ChangeInput.cs
@@ -10,7 +10,17 @@ public class ChangeInput : MonoBehaviour {
     public Text third;
     public Text fourth;
 
+    public Text result;
+    public Text randomNumber;
+
     int focusedIndex = 0;
+    string actualNumber;
+
+    System.Random ran = new System.Random();
+
+    ChangeInput() {
+        actualNumber = random();
+    }
 
     public void ChangeInputValue(int value) {
         Debug.Log("new value " + value.ToString());
@@ -29,12 +39,63 @@ public class ChangeInput : MonoBehaviour {
                 fourth.text = value.ToString();
                 break;
         }
-
-
+        focusedIndex++;
     }
 
     public void ChangeInputIndex(int index)
     {
         focusedIndex = index;
+    }
+
+    public void CheckScore()
+    {
+        string guess = first.text + second.text + third.text + fourth.text;
+        result.text = findPoint(actualNumber, guess);
+        randomNumber.text = actualNumber;
+    }
+
+    private string random()
+    {
+        int[] numbers = new int[] {1,2,3,4,5,6,7,8,9};
+        int _min = 0;
+        int _max = numbers.Length;
+
+        for (int i = 0; i < 20; i++)
+        {
+            int i1 = ran.Next(_min, _max);
+            int i2 = ran.Next(_min, _max);
+
+            int temp = numbers[i1];
+            numbers[i1] = numbers[i2];
+            numbers[i2] = temp;
+        }
+        return numbers[0] + "" + numbers[1] + "" + numbers[2] + "" + numbers[3]; // i know its ugly
+    }
+
+
+    private string findPoint(string number, string guessNumber)
+    {
+        int plusPoint = 0;
+        int minusPoint = 0;
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] == guessNumber[i])
+            {
+                plusPoint++;
+            }
+            else
+            {
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (number[i] == guessNumber[j])
+                    {
+                        minusPoint++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ("+" + plusPoint + "  " + " - " + minusPoint);
     }
 }
