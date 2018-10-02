f61e196122360b941ec25df9e783511d39af639d
Winlentia <winlentia@gmail.com>
2018-10-02 19:46:49 +0300

    router think

---
+++ Assets/script/MainSceneRouter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainSceneRouter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene ("SampleScene", LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playButtonOnClick(){
+		
+		//SceneManager.LoadScene ("SampleScene", LoadSceneMode.Additive);
+		Debug.Log ("inside the method");
+	}
+
+	
+}
